3ec395f1066317bb568595e7e8b59c46c45cece0
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-01-28 18:24:11 +0000

    rework directory

--- src/main.js
+++ src/main.js
@@ -3,3 +3,67 @@
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+function __init__() {
+    
+}
+
+/*----- Classes -----*/
+
+class Game {
+    constructor(players, rules) {
+        gameDeck = new Deck()
+        this.players = [];
+        this.deck = gameDeck // Just to reference it easier
+        this.rules = {};
+    }
+    
+    play() {
+            
+        }
+    nextPlayer() {
+        
+    }
+    skipPlayer() {
+        
+    }
+    reversePlayer() {
+        
+    }
+    drawCards() {
+        
+    }
+    battleForUno() {
+        
+    }
+}
+
+class Rules {
+    
+}
+
+class Player {
+    constructor(name) {
+    this.name = name;
+    this.hand = [];
+    this.turn = false;
+  }
+}
+
+class Deck {
+    shuffle() {
+        
+    }    
+}
+
+class Card {
+    
+}
+
+class Pile {
+    
+}
+
+class Rules {
+    
+}
